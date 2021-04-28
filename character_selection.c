431f004ab9781d007bcb66e0a37d4dbc16f8401f
Oscar Keren <oscar.keren@epfl.ch>
2021-04-28 16:48:25 +0200

    RGB de tous les persos

--- character_selection.c
+++ character_selection.c
@@ -31,24 +31,71 @@ static THD_FUNCTION(CharacterSelection, arg) {
 		time = chVTGetSystemTime();
 
 
-
+	// Faudra faire attention aux Magic numbers avec les RGB je pense
 		switch(get_selector()) {
 						case 0: // Mario
-							set_rgb_led(LED2, 8,70,158);
-							set_rgb_led(LED4, 8,70,158);
-							set_rgb_led(LED6, 194,53,45);
-							set_rgb_led(LED8, 194,53,45);
+							set_rgb_led(LED2, 100,0,0);
+							set_rgb_led(LED4, 100,0,0);
+							set_rgb_led(LED6, 0,100,100);
+							set_rgb_led(LED8, 0,100,100);
 
 						break;
 
 						case 1: // Luigi
-							set_rgb_led(LED2, 24,222,44);
-							set_rgb_led(LED4, 24,222,44);
-							set_rgb_led(LED6, 21,35,255);
-							set_rgb_led(LED8, 21,35,255);
+							set_rgb_led(LED2, 9,87,17);
+							set_rgb_led(LED4, 9,87,17);
+							set_rgb_led(LED6, 8,13,100);
+							set_rgb_led(LED8, 8,13,100);
 
 						break;
 
+						case 2: // Peach
+							set_rgb_led(LED2, 94,57,74);
+							set_rgb_led(LED4, 94,57,74);
+							set_rgb_led(LED6, 33,73,82);
+							set_rgb_led(LED8, 33,73,82);
+
+						break;
+						
+						case 3: // Daisy
+							set_rgb_led(LED2, 100,100,20);
+							set_rgb_led(LED4, 100,100,20);
+							set_rgb_led(LED6, 80,40,0);
+							set_rgb_led(LED8, 80,40,0);
+
+						break;
+
+						case 4: // Toad
+							set_rgb_led(LED2, 95,14,6);
+							set_rgb_led(LED4, 95,14,6);
+							set_rgb_led(LED6, 97,97,97);
+							set_rgb_led(LED8, 97,97,97);
+
+						break;
+						
+						case 5: // Yoshi
+							set_rgb_led(LED2, 1,86,0);
+							set_rgb_led(LED4, 1,86,0);
+							set_rgb_led(LED6, 96,96,96);
+							set_rgb_led(LED8, 96,96,96);
+
+						break;
+
+						case 6: // Wario
+							set_rgb_led(LED2, 94,100,8);
+							set_rgb_led(LED4, 94,100,8);
+							set_rgb_led(LED6, 61,13,98);
+							set_rgb_led(LED8, 61,13,98);
+
+						break;
+						
+						case 7: // Waluigi
+							set_rgb_led(LED2, 61,13,98);
+							set_rgb_led(LED4, 61,13,98);
+							set_rgb_led(LED6, 0,0,25);
+							set_rgb_led(LED8, 0,0,25);
+
+						break;
 
 		}
 
