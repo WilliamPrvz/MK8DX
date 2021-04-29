bbff412840ba489d4d0910d0ee326b7a4ab8c3cf
Oscar Keren <oscar.keren@epfl.ch>
2021-04-29 16:25:19 +0200

    LEDS + enum

--- character_selection.c
+++ character_selection.c
@@ -21,7 +21,7 @@
 #include <sdio.h>
 
 
-
+//FAIRE DES COMMENTAIRES PARTOUT
 
 static THD_WORKING_AREA(waCharacterSelection, 256);
 static THD_FUNCTION(CharacterSelection, arg) {
@@ -37,9 +37,6 @@ static THD_FUNCTION(CharacterSelection, arg) {
 
     int old_status = 0;
     
-    
-		
-//	int k = 0;
 	while (1){
 		
 		int status = get_selector();
@@ -53,91 +50,91 @@ static THD_FUNCTION(CharacterSelection, arg) {
 			switch(status) {
 
 
-						case 0: // Mario
+						case Mario:
 
 
 
 							playSoundFile("Son_MK8DX/itsame_mario.wav", SF_FORCE_CHANGE);
 
 							set_rgb_led(LED2, 100,0,0);
-							set_rgb_led(LED4, 100,0,0);
-							set_rgb_led(LED6, 0,0,100);
+							set_rgb_led(LED4, 0,0,100);
+							set_rgb_led(LED6, 100,0,0);
 							set_rgb_led(LED8, 0,0,100);
 
 						break;
 
-						case 1: // Luigi
+						case Luigi:
 						
 							playSoundFile("Son_MK8DX/luigi_ohyeahluigi.wav", SF_FORCE_CHANGE);
 							
-							set_rgb_led(LED2, 9,87,17);
-							set_rgb_led(LED4, 9,87,17);
-							set_rgb_led(LED6, 8,13,100);
+							set_rgb_led(LED2, 0,100,0);
+							set_rgb_led(LED4, 8,13,100);
+							set_rgb_led(LED6, 0,100,0);
 							set_rgb_led(LED8, 8,13,100);
 
 						break;
 
-						case 2: // Peach
+						case Peach:
 						
 							playSoundFile("Son_MK8DX/peach_peach.wav", SF_FORCE_CHANGE);
 						
 							set_rgb_led(LED2, 94,57,74);
-							set_rgb_led(LED4, 94,57,74);
-							set_rgb_led(LED6, 33,73,82);
+							set_rgb_led(LED4, 33,73,82);
+							set_rgb_led(LED6, 94,57,74);
 							set_rgb_led(LED8, 33,73,82);
 
 						break;
 
-						case 3: // Daisy
+						case Daisy:
 						
 							playSoundFile("Son_MK8DX/daisy_yeahdaisy.wav", SF_FORCE_CHANGE);
 						
 							set_rgb_led(LED2, 100,100,20);
-							set_rgb_led(LED4, 100,100,20);
-							set_rgb_led(LED6, 80,40,0);
+							set_rgb_led(LED4, 80,40,0);
+							set_rgb_led(LED6, 100,100,20);
 							set_rgb_led(LED8, 80,40,0);
 
 						break;
 
-						case 4: // Toad
+						case Toad:
 						
 							playSoundFile("Son_MK8DX/toad.wav", SF_FORCE_CHANGE);
 							set_rgb_led(LED2, 95,14,6);
-							set_rgb_led(LED4, 95,14,6);
-							set_rgb_led(LED6, 97,97,97);
+							set_rgb_led(LED4, 97,97,97);
+							set_rgb_led(LED6, 95,14,6);
 							set_rgb_led(LED8, 97,97,97);
 
 						break;
 
-						case 5: // Yoshi
+						case Yoshi:
 						
 							playSoundFile("Son_MK8DX/yoshi.wav", SF_FORCE_CHANGE);
 							
 							set_rgb_led(LED2, 1,86,0);
-							set_rgb_led(LED4, 1,86,0);
-							set_rgb_led(LED6, 96,96,96);
+							set_rgb_led(LED4, 96,96,96);
+							set_rgb_led(LED6, 1,86,0);
 							set_rgb_led(LED8, 96,96,96);
 
 						break;
 
-						case 6: // Wario
+						case Wario:
 						
 							playSoundFile("Son_MK8DX/wario_wariotime.wav", SF_FORCE_CHANGE);
 							
-							set_rgb_led(LED2, 94,100,8);
+							set_rgb_led(LED2, 100,100,0);
 							set_rgb_led(LED4, 94,100,8);
-							set_rgb_led(LED6, 61,13,98);
-							set_rgb_led(LED8, 61,13,98);
+							set_rgb_led(LED6, 100,100,0);
+							set_rgb_led(LED8, 94,100,8);
 
 						break;
 
-						case 7: // Waluigi
+						case Waluigi:
 						
 							playSoundFile("Son_MK8DX/waluigi_waluigi.wav", SF_FORCE_CHANGE);
 							
 							set_rgb_led(LED2, 61,13,98);
-							set_rgb_led(LED4, 61,13,98);
-							set_rgb_led(LED6, 0,0,25);
+							set_rgb_led(LED4, 0,0,25);
+							set_rgb_led(LED6, 61,13,98);
 							set_rgb_led(LED8, 0,0,25);
 
 						break;
