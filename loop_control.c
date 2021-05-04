489490cc5c69231d96b3ae0f9d1885e889c36524
Oscar Keren <oscar.keren@epfl.ch>
2021-05-04 09:16:00 +0200

    circuit-completed

--- loop_control.c
+++ loop_control.c
@@ -12,7 +12,7 @@
 #include <chprintf.h>
 #include "usbcfg.h"
 
-static unsigned int loop_number = 0;
+static bool circuit_completed = false; // becomes true once 3 loops are completed
 
 static THD_WORKING_AREA(waLoopControl, 256);
 static THD_FUNCTION(LoopControl, arg) {
@@ -21,22 +21,29 @@ static THD_FUNCTION(LoopControl, arg) {
 	(void)arg;
 
 	systime_t time;
+	
+	unsigned int loop_number = 0;
 
 	while (1){
 		time = chVTGetSystemTime();
+		
+		if ((get_prox(IR_RIGHT) > PROXIMITY_THRESHOLD)&&(get_prox(IR_LEFT) > PROXIMITY_THRESHOLD)){
+			
+			++loop_number;
+			chThdSleepMilliseconds(1000); // makes sure that it increments loop_number only once
 
-//		if ((get_prox(IR_RIGHT)<10)&&(get_prox(IR_LEFT)<10)){
-//			loopNb ++;
-//			chThdSleepMilliseconds(1000);
-//
-//		}
-//
-//		if (loopNb >= 3){
-//
-//		}
+		}
+
+		//if (loop_number >= 3){
+		
+			//circuit_completed = true;
+
+		//}
 
 	chprintf((BaseSequentialStream *)&SDU1, "Right : %4d, \r\n", get_prox(IR_RIGHT));
 	chprintf((BaseSequentialStream *)&SDU1, "Left : %4d,\r\n", get_prox(IR_LEFT));
+	
+	chprintf((BaseSequentialStream *)&SDU1, "loop_number : %f, \r\n", loop_number);
 
 
 		chThdSleepUntilWindowed(time, time + MS2ST(10));
@@ -48,4 +55,8 @@ static THD_FUNCTION(LoopControl, arg) {
 
 void loop_control_start(void){
 	chThdCreateStatic(waLoopControl, sizeof(waLoopControl), NORMALPRIO, LoopControl, NULL);
+}
+
+bool get_circuit_completed(void) {
+	return circuit_completed;
 }
--- loop_control.h
+++ loop_control.h
@@ -8,11 +8,11 @@
 #ifndef LOOP_CONTROL_H_
 #define LOOP_CONTROL_H_
 
-#define PROXIMITY_THRESHOLD 	100 //5cm
+#define PROXIMITY_THRESHOLD 	100 //corresponds to around 5cm
 
 
 void loop_control_start(void);
-
+bool get_circuit_completed(void);
 
 
 #endif /* LOOP_CONTROL_H_ */
