4a1d9761855dce38eb440c1573e464dd889515e8
WilliamPrvz <william.provenaz@epfl.ch>
2021-05-10 14:01:01 +0200

    mirror ajustement

--- main.h
+++ main.h
@@ -20,8 +20,8 @@ extern "C" {
 #define GOAL_DISTANCE 			10.0f
 #define MAX_DISTANCE 			25.0f
 #define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
-#define KP						400.0f
-#define KI 						3.5f	//must not be zero
+#define KP						800.0f
+#define KI 						10.0f	//must not be zero
 #define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
 #define IR_LEFT					5
 #define IR_RIGHT				2
--- process_image.c
+++ process_image.c
@@ -110,8 +110,8 @@ static THD_FUNCTION(CaptureImage, arg) {
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
 
-	//Takes pixels 0 to IMAGE_BUFFER_SIZE of the line 479 + 480 (minimum 2 lines because reasons)
-	po8030_advanced_config(FORMAT_RGB565, 0, 479, IMAGE_BUFFER_SIZE, 2, SUBSAMPLING_X1, SUBSAMPLING_X1);
+	//Takes pixels 0 to IMAGE_BUFFER_SIZE of the line 239 + 240 (minimum 2 lines because reasons)
+	po8030_advanced_config(FORMAT_RGB565, 0, 239, IMAGE_BUFFER_SIZE, 2, SUBSAMPLING_X1, SUBSAMPLING_X1);
 	dcmi_enable_double_buffering();
 	dcmi_set_capture_mode(CAPTURE_ONE_SHOT);
 	dcmi_prepare();
